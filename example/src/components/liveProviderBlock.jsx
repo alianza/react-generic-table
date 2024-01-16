d23aee55ce7d1e029a93bba2a376e5c464dc03b4
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-01-16 17:51:45 +0100

    Added liveProviderBlock.jsx component

---
+++ example/src/components/liveProviderBlock.jsx
@@ -0,0 +1,35 @@
+import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
+import utilStyles from "../utils.module.scss";
+import { ArrowPathIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
+
+export default function LiveProviderBlock({ scope, code = "" }) {
+  const [codeState, setCodeState] = useState(code);
+
+  useEffect(() => {
+    setCodeState(code);
+  }, [code]);
+
+  const resetCode = () => setCodeState(code);
+
+  return (
+    <div className="flex flex-col items-center gap-8 overflow-y-hidden py-4">
+      <LiveProvider scope={scope} code={codeState}>
+        <div className="relative">
+          <LiveEditor className={utilStyles.codeEditor} onChange={setCodeState} />
+          {codeState !== code && (
+            <button className="absolute right-2 top-2">
+              <ArrowPathIcon
+                className="w-8 transition-transform hover:scale-105 active:scale-95 active:duration-150"
+                title="Reset code"
+                onClick={resetCode}
+              />
+            </button>
+          )}
+        </div>
+        <LiveError />
+        <LivePreview className="drop-shadow-xl" />
+      </LiveProvider>
+    </div>
+  );
+}
