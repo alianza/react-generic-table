6b0a3d04a8a0772f7e3519b840735ad358a0e135
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-01-16 19:35:01 +0100

    Removed codeBlock.js component

--- example/src/components/codeBlock.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function CodeBlock({ children, style = {}, className = '' }) {
-  return (
-    <pre style={{...style, whiteSpace: 'break-spaces'}} className={className}>
-      <code>{children}</code>
-    </pre>
-  );
-}
